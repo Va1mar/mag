8b6fe1eb518efb7ce696c17bdc83bb6c2a8a0df2
Va1mar <vova.1123.v@mail.ru>
2018-11-26 16:09:02 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+// Общие настройки
+
+
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+// Подключение файлов
+define('ROOT', dirname(__FILE__));
+//require_once ROOT.'/components/Autoload.php';
+
+spl_autoload_register(function($class_name) {
+    $paths = array(
+        '/components/',
+        '/models/'
+    );
+    
+    foreach($paths as $path) {
+        $filename = ROOT.$path.$class_name.'.php';
+        if(is_file($filename))
+            require_once $filename;
+    }
+});
+
+
+// Работа роутера
+
+$router = new Router();
+$router->run();
+
+?>
